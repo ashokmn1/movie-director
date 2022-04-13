48be61a6e101cb3a85aea5d8f3ea781cefeaba22
Ashok M N <ashok.n.19@mountblue.tech>
2022-04-13 19:59:56 +0530

    updated

--- index.js
+++ index.js
@@ -14,20 +14,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
+  const corsWhiteList = ['http://localhost:3000', "https://imdbapiproject.herokuapp.com"]
+  if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  //res.setHeader('Access-Control-Allow-Credentials', true);
-
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+  }
   // Pass to next layer of middleware
   next();
 });
