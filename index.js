42e4d67fb9314b218a683eebf92d1fffe4c9974f
Ashok M N <72309703+ashokmn1@users.noreply.github.com>
2022-04-13 19:47:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
   if (corsWhiteList.indexOf(req.headers.origin) !== -1){
  
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
